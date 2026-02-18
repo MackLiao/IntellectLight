70d72c9cfd3569f74bb8024918fb52fc2f74a747
MackLiao <g.liao@wustl.edu>
2026-02-18 12:21:00 -0600

    feat: add main application class
    - Create Spring Boot application entry point

---
+++ src/main/java/com/tongji/IntellectLightApplication.java
@@ -0,0 +1,12 @@
+package com.tongji;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntellectLightApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IntellectLightApplication.class, args);
+    }
+}
